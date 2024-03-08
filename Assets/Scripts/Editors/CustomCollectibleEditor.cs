2fc267210fe0f4e649d8dc7f0c232b4e8b840f38
dm-2028 <michlig.david@gmail.com>
2024-03-08 00:59:44 -0600

    added function to pull in collectibles set height of collectibles in editor

--- Assets/Scripts/Editors/CustomCollectibleEditor.cs
+++ Assets/Scripts/Editors/CustomCollectibleEditor.cs
@@ -5,6 +5,7 @@ using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(Collectable), true)]
+[CanEditMultipleObjects]
 public class CustomCollectibleEditor : Editor
 {
     private SerializedProperty _serializationId;
@@ -13,10 +14,11 @@ public class CustomCollectibleEditor : Editor
     {
         var collectables = Collectable.FindAll();
         var last = collectables.LastOrDefault();
+        Debug.Log("last " + last);
         var nextId = last == null ? 0 : last.serializationId + 1;
 
         var exists = new HashSet<int>(collectables.Select(o => o.serializationId));
-        for(int i = 0; i < FindNextId(); i++)
+        for(int i = 0; i < nextId; i++)
         {
             if (!exists.Contains(i))
                 return i;
@@ -28,7 +30,10 @@ public class CustomCollectibleEditor : Editor
     {
         var collectable = (Collectable)target;
         var isPrefab = PrefabUtility.GetCorrespondingObjectFromSource(collectable.gameObject) == null;
-
+        LayerMask layerMask = -1;
+        if(Physics.Raycast(collectable.transform.position, Vector3.down, out RaycastHit hit, 2f, layerMask, QueryTriggerInteraction.Ignore)){
+            collectable.transform.position = new(collectable.transform.position.x, hit.point.y + .5f, collectable.transform.position.z);
+        }
         if(!isPrefab && collectable.serializationId == -1)
         {
             collectable.serializationId = FindNextId();
--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -40,13 +40,38 @@ public abstract class StateMachine : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-
-        currentState?.EvaluateSubmergence(other);
+        Debug.Log("on trigger enter " + other.gameObject.tag);
+        if (other.gameObject.tag == ("Collectable"))
+        {
+            Debug.Log("trigger is tagged");
+            Collectable collectable = other.gameObject.GetComponent<Collectable>();
+            StartCoroutine(PullCollectable(collectable));
+        }
+        else
+        {
+            currentState?.EvaluateSubmergence(other);
+        }
 
     }
 
     private void OnTriggerStay(Collider other)
     {
         currentState?.EvaluateSubmergence(other);  
+    }
+
+    IEnumerator PullCollectable(Collectable collectable)
+    {
+        Debug.Log("start coroutine");
+        float i = 0f;
+        float rate = 1.0f / 3.0f;
+
+        while(transform.position != collectable.transform.position)
+        {
+            i += Time.deltaTime / rate;
+            collectable.transform.position = Vector3.Lerp(collectable.transform.position, transform.position, i);
+            yield return null;
+        }
+        collectable.collected = true;
+        collectable.gameObject.SetActive(false);
     }
 }
